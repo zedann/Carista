629deb67661ffba047a9b5272e586e7eb287b41d
Zedan <zedanmohamedcp@gmail.com>
2025-04-22 23:07:40 +0200

    chore:car.controller->createCar

--- server/src/controllers/car.controller.ts
+++ server/src/controllers/car.controller.ts
@@ -1,7 +1,29 @@
 import { NextFunction, Request, Response } from "express";
+import { prisma } from "../database";
+import { HttpStatus } from "../constants/httpStatus";
 
-export const createCar = async (req: Request, res: Response, next: NextFunction) => { }
-export const getAllCars = async (req: Request, res: Response, next: NextFunction) => { }
+export const createCar = async (req: Request, res: Response, next: NextFunction) => {
+  const { make, model, year, kilometers, images, lastService } = req.body;
+  const car = await prisma.car.create(
+    {
+      data: {
+        make,
+        model,
+        year,
+        kilometers,
+        images,
+        lastService: lastService ? new Date(lastService) : undefined,
+      }
+    }
+  )
+
+  res.status(HttpStatus.CREATED).json({
+    message: "Car Created Succefully",
+    data: car
+  })
+}
+export const getAllCars = async (req: Request, res: Response, next: NextFunction) => {
+}
 export const getCarById = async (req: Request, res: Response, next: NextFunction) => { }
 export const updateCar = async (req: Request, res: Response, next: NextFunction) => { }
 export const deleteCar = async (req: Request, res: Response, next: NextFunction) => { }
